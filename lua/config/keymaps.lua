a9c9561e9379492d794e62ac622de70ad077ba83
Kishandharan <kishanrajeevi.sangamone@gmail.com>
2025-12-31 18:45:25 +0400

    Added some stuff

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set('v', 'kj', '<ESC>', opts)
+vim.keymap.set('i', 'kj', '<ESC>', opts)
+vim.keymap.set('c', 'kj', '<ESC>', opts)
+vim.keymap.set('x', 'kj', '<ESC>', opts)
+vim.keymap.set('s', 'kj', '<ESC>', opts)
+vim.keymap.set('o', 'kj', '<ESC>', opts)
